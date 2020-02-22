35054615e0d14adcf4193ed2a2f72a360c0ba5aa
Nikheel Savant <nikheelsavant@Nikheels-MacBook-Pro.local>
2020-02-22 13:27:27 -0800

    count_bits

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,8 +1,15 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+    short numOfSetBits = 0;
+    while (x) {
+        if(x % 2) {
+            numOfSetBits++;
+        }
+        x /= 2;
+    }
+    
+    return numOfSetBits;
 }
 
 int main(int argc, char* argv[]) {
