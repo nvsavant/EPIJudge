72098471a2db9f82f74cf239253bf6940e3cee89
Nikheel Savant <nikheelsavant@Nikheels-MacBook-Pro.local>
2020-02-23 10:05:55 -0800

    Naive Solution

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -1,10 +1,20 @@
 #include <vector>
+#include <cfloat>
 
 #include "test_framework/generic_test.h"
 using std::vector;
 double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+    double maxProfit = DBL_MIN;
+    
+    for(int i = 0; i < prices.size() - 1; i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            if(prices[j] - prices[i] > maxProfit) {
+                maxProfit = prices[j] - prices[i];
+            }
+        }
+    }
+    
+    return maxProfit;
 }
 
 int main(int argc, char* argv[]) {
