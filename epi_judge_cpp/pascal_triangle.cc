7db77e34d952e83cb1eecf304efb7de78406b7f5
Nikheel Savant <nikheelsavant@Nikheels-MacBook-Pro.local>
2020-02-23 14:57:07 -0800

    pascal_triangle

--- epi_judge_cpp/pascal_triangle.cc
+++ epi_judge_cpp/pascal_triangle.cc
@@ -3,8 +3,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<vector<int>> GeneratePascalTriangle(int num_rows) {
-  // TODO - you fill in here.
-  return {};
+    if(num_rows == 0) {
+        return {};
+    }
+    
+    vector<vector<int>> retVec;
+    for (int line = 1; line <= num_rows; line++) {
+        vector<int> vec;
+        long c = 1;
+        for (int i = 1; i <= line; i++) {
+            vec.push_back(c);
+            c = c * (line - i) / i;
+        }
+        retVec.push_back(vec);
+    }
+    
+    return retVec;
 }
 
 int main(int argc, char* argv[]) {
